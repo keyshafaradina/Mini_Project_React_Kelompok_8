d480c0bbb6d9d422a6c541a3fa056fcdf1329e2a
elgalumbangaoll-commits <elgalumbangaoll@student.ub.ac.id>
2026-03-24 07:05:43 -0700

    Fix routing App.js dan path elga

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Elga from './pages/elga'; // Pastikan path-nya benar ke file elga.jsx kamu
+import Elga from './pages/elga'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Ini yang bikin localhost:3000/elga bisa munculin profilmu */}
+       
+        <Route path="/" element={
+          <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-pink-500">Welcome to Group 8 Project</h1>
+          </div>
+        } />
+
+        
         <Route path="/elga" element={<Elga />} />
         
-        {/* Kamu bisa tambah route temenmu di sini nanti */}
-        {/* <Route path="/nama-temen" element={<Temen />} /> */}
+    
       </Routes>
     </Router>
   );
