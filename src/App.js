fb20210433a22e99beed2f03c31a558c3c2d5c93
belqisnblht <belqisnblht@student.ub.ac.id>
2026-03-24 22:57:30 +0800

    halaman profil belqis

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Belqis from "./pages/belqis";
-import Elga from "./pages/Elga";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/belqis" element={<Belqis />} />
-        <Route path="/elga" element={<Elga />} />
-      </Routes>
+      <div className="p-5">
+
+        {/* Navbar sederhana */}
+        <nav className="mb-5">
+          <Link to="/" className="mr-4 text-blue-500">Home</Link>
+          <Link to="/belqis" className="text-blue-500">Profil Belqis</Link>
+        </nav>
+
+        <Routes>
+          {/* Halaman Home */}
+          <Route path="/" element={
+            <h1 className="text-3xl font-bold text-center">
+              Selamat Datang di Website Profil
+            </h1>
+          } />
+
+          {/* Halaman Profil */}
+          <Route path="/belqis" element={<Belqis />} />
+        </Routes>
+
+      </div>
     </BrowserRouter>
   );
 }
